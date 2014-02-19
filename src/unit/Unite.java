9ce36220fc26f3059486557cd618c029043a4205
lecpie <lecapierre@wanadoo.fr>
2014-02-19 11:00:06 +0100

    Les unitees ont dirrectement acces a leurs coordonnees

--- src/unit/Unite.java
+++ src/unit/Unite.java
@@ -1,11 +1,31 @@
 package unit;
 
+import map.Coordonnee;
+
 public abstract class Unite {
+	
+	private Coordonnee coord;
 	
 	/**
 	 * Recupere le nombre de deplacement possible pour le tour actuel
 	 * @return le nombre de deplacement
 	 */
 	public abstract int getNbDeplacement();
+	
+	/**
+	 * Renvoie les coordonnees de l'unite
+	 * @return les coordonnees de l'unite
+	 */
+	public Coordonnee getCoordonnee() {
+		return coord;
+	}
+	
+	/**
+	 * Change les coordonnees de l'unite
+	 * @param coord les nouvelles coordonnees de l'unite
+	 */
+	public void setCoordonnee (Coordonnee coord) {
+		this.coord = coord;
+	}
 	
 }
