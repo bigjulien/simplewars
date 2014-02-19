1a7e97ff7ce0034984d87952329b7d0749c5a82e
lecpie <lecapierre@wanadoo.fr>
2014-02-19 10:53:14 +0100

    Creation d'une classe abstraite d'unite et representation de coordonnees dans la grille

---
+++ src/unit/Unite.java
@@ -0,0 +1,11 @@
+package unit;
+
+public abstract class Unite {
+	
+	/**
+	 * Recupere le nombre de deplacement possible pour le tour actuel
+	 * @return le nombre de deplacement
+	 */
+	public abstract int getNbDeplacement();
+	
+}
