5f8184517cd23a49cdd6bf24b5dbcbee97e57c61
Pierre LECA <lecapierre@wanadoo.fr>
2014-03-05 12:16:25 +0100

    ?

--- src/unit/Archer.java
+++ src/unit/Archer.java
@@ -3,7 +3,7 @@ package unit;
 public class Archer extends Unite {
     private static final String imageSoldat = "Units/2673.gif";
     private static final int NBDEPLACEMENT = 1;
-    
+        
     public Archer(){
         setBufferedImage(imageSoldat);
     }
