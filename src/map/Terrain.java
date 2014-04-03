2c11d6f2aefb1d2db2c9b34fe27a5191820589cd
Benjamin CLAQUIN <claquin@polytech.unice.fr>
2014-04-03 09:22:59 +0200

    B Images champ deplacement B images ennemi allié

--- src/map/Terrain.java
+++ src/map/Terrain.java
@@ -1,15 +1,36 @@
 package map;
 
 import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.File;
 
-public class Terrain {
+import javax.imageio.ImageIO;
+
+public abstract class Terrain {
 	
 	private Color couleur;
 	private boolean praticable;
+	private BufferedImage bufferedImage;
+	
 	
 	public Terrain (Color couleur, boolean praticable) {
 		this.couleur = couleur;
 		this.praticable = praticable;
+	}
+	
+	public abstract String getPathImage();
+	
+	public Terrain (boolean particable){
+	    this.praticable=praticable;
+        
+        String path =  getPathImage();
+
+        try {
+            setBufferedImage(ImageIO.read(new File(path)));
+            
+        } catch (Exception e) {
+            System.out.println("Fichier manquant : " + path);
+        }    
 	}
 	
 	public Color getCouleur() {
@@ -27,4 +48,21 @@ public class Terrain {
 	    String praticabilite = praticable ? "praticable" : "impraticable";
 	    return "terrain "+praticabilite;
 	}
+
+    public BufferedImage getBufferedImage() {
+        return bufferedImage;
+    }
+
+    public void setBufferedImage(BufferedImage bufferedImage) {
+        this.bufferedImage = bufferedImage;
+    }
+    
+    public void setBufferedImage(String path){
+        try {
+            setBufferedImage(ImageIO.read(new File(path)));
+            
+        } catch (Exception e) {
+            System.out.println("Fichier manquant : " + path);
+        }    
+    }
 }
